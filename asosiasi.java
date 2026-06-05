e5d6455c7f4d34ea80ec3bad82be52504b3526e7
kawannainggolan <kawannainggolan19@gmail.com>
2026-06-05 16:04:31 +0700

    membuat class dosen didalam mahasiswa

--- asosiasi.java
+++ asosiasi.java
@@ -26,3 +26,45 @@ class MataKuliah {
     return kode + " " + nama + " (" + sks + " SKS)";
     }
 }
+
+class Dosen extends Orang {
+    private String nidn;
+    private ArrayList<Matakuliah> mataKuliahDijajar;
+
+    public Dosen(String nama, String nidn) {
+    this.nidn = nidn;
+    this.mataKuliahDijajar = new ArrayList<>();
+    }
+
+    public void tampilkanDijajar() {
+    System.out.println(getNama() + " berperan sebagai Dosen.");
+    }
+
+    public void tampilkanMatakuliahDijajar() {
+    System.out.println("Dosen : " + getNama());
+    System.out.println("NIDN : " + nidn);
+    System.out.println("Mata Kuliah yang Dijajar:");
+    for (Matakuliah mk : mataKuliahDijajar) {
+    System.out.println("-- " + mk.getInfo());
+    }
+    }
+}
+
+    class Mahasiswa extends Orang {
+    private String nidn;
+    private ArrayList<Matakuliah> mataKuliahDiambil;
+
+    public Mahasiswa(String nama, String nidn) {
+    super(nama);
+    this.nidn = nidn;
+    this.mataKuliahDiambil = new ArrayList<>();
+    }
+
+    public void tampilkanMatakuliah() {
+    System.out.println(getNama() + " berperan sebagai Mahasiswa.");
+    }
+
+    public void tampilkanPeran() {
+    System.out.println(getNama() + " berperan sebagai Mahasiswa.");
+}
+}
